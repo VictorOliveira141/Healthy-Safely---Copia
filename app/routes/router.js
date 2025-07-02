2116f1f1f42e2acdf81802a702fda2d0312d4a32
VictorOliveira141 <rm94501@estudante.fieb.edu.br>
2025-07-02 23:36:00 +0000

    Mensagem de sucesso ao fazer login (ajustado)

--- app/routes/router.js
+++ app/routes/router.js
@@ -2,110 +2,103 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-const usuarios = []
+const usuarios = [];
 
 /*Criação de rotas*/
-router.get("/", (req, res)=>{
-    res.render("pages/safely");
-})
+router.get("/", (req, res) => {
+  res.render("pages/safely");
+});
 
-router.get("/safely", (req, res)=>{
-    res.render("pages/safely");
-})
+router.get("/safely", (req, res) => {
+  res.render("pages/safely");
+});
 
-router.get("/login", (req, res)=>{
-    res.render("pages/login",
-        { erro: null,
-          sucesso: false,
-        
-        }
-    );
-})
+router.get("/login", (req, res) => {
+  res.render("pages/login", { erro: null, sucesso: false });
+});
 
-router.get("/cadastro", (req, res)=>{
-    res.render("pages/cadastro", {
-        "erros":null, 
-        "valores":{
-            "nome": "",
-            "email": "",
-            "senha": "",
-            "confirmar-senha": ""}, 
-        "retorno":null},
-    )
-})
+router.get("/cadastro", (req, res) => {
+  res.render("pages/cadastro", {
+    erros: null,
+    valores: {
+      nome: "",
+      email: "",
+      senha: "",
+      "confirmar-senha": "",
+    },
+    retorno: null,
+  });
+});
 
-router.get("/tela-inicial", (req, res)=>{
-    res.render("pages/tela-inicial")
-})
+router.get("/tela-inicial", (req, res) => {
+  res.render("pages/tela-inicial");
+});
 /*fim*/
 
 /*Criação de validações*/
-router.post("/cadastro", 
+router.post(
+  "/cadastro",
 
-    body("nome").trim().isLength({min:3, max:50}).withMessage("*O Nome de usuário deve conter entre 3 e 50 caracteres!")
-    .matches(/^[A-Za-zÀ-ú\s]+$/).withMessage("O nome deve conter apenas letras!"),
-    
-    body("email").isEmail().withMessage("*Endereço de email inválido!"),
+  body("nome")
+    .trim()
+    .isLength({ min: 3, max: 50 })
+    .withMessage("*O Nome de usuário deve conter entre 3 e 50 caracteres!")
+    .matches(/^[A-Za-zÀ-ú\s]+$/)
+    .withMessage("O nome deve conter apenas letras!"),
 
-    body("senha").isLength({min:8}).withMessage("*Digite uma senha maior que 6 digitos!"),
+  body("email").isEmail().withMessage("*Endereço de email inválido!"),
 
-    body("confirmar-senha").isLength({min:8}).withMessage("*As senha não confere!")
-    .custom( (value, { req } ) => {
-        if (value !== req.body.senha){
-            throw new Error("As senhas não conferem!")
-        }
-        return true;
+  body("senha")
+    .isLength({ min: 8 })
+    .withMessage("*Digite uma senha maior que 8 digitos!"),
+
+  body("confirmar-senha")
+    .isLength({ min: 8 })
+    .withMessage("*As senha não confere!")
+    .custom((value, { req }) => {
+      if (value !== req.body.senha) {
+        throw new Error("As senhas não conferem!");
+      }
+      return true;
     }),
 
-    function(req, res){
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            console.log(errors);
-            return res.render("pages/cadastro", {
-                "erros": errors, 
-                "valores": req.body, 
-                "retorno":null}
-            );
-        }
-        
-        usuarios.push({
-            email: req.body.email,
-            senha: req.body.senha
-        });
-        
-        res.redirect("/login");
+  function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.render("pages/cadastro", {
+        erros: errors,
+        valores: req.body,
+        retorno: null,
+      });
     }
-)
+
+    usuarios.push({
+      email: req.body.email,
+      senha: req.body.senha,
+    });
+
+    res.redirect("/login");
+  }
+);
 
 router.post("/login", (req, res) => {
-    const { usuarioDigitado, senhaDigitada } = req.body;
+  const { usuarioDigitado, senhaDigitada } = req.body;
 
-    const usuarioEncontrado = usuarios.find(u =>
-        u.email === usuarioDigitado && u.senha === senhaDigitada
-    );
+  const usuarioEncontrado = usuarios.find(
+    (u) => u.email === usuarioDigitado && u.senha === senhaDigitada
+  );
 
-    if (usuarioEncontrado) {
-        return res.render("pages/login", {erro:null, sucesso: true});
-    } else {
-        return res.render("pages/login", { 
-            erro: "*Não reconhecemos estas credenciais. Tente novamente.", 
-            sucesso: false,
-         });
-    }
+  if (usuarioEncontrado) {
+    return res.render("pages/login", { erro: null, sucesso: true });
+  } else {
+    return res.render("pages/login", {
+      erro: "*Não reconhecemos estas credenciais. Tente novamente.",
+      sucesso: false,
+    });
+  }
 });
 
-
-
 /*Fim*/
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
